3b1175ce67a74e6211d6b763432b0aade0f6be3e
Rusya <85542333+Rusya1465@users.noreply.github.com>
2024-11-02 10:35:42 +0600

    Update firebase-config.js
    Fb changes

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -7,12 +7,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAReovOYiaL02LF8QpjejpL1DXLYqQDVkA",
-  authDomain: "expense-tracker2-cf7fb.firebaseapp.com",
-  projectId: "expense-tracker2-cf7fb",
-  storageBucket: "expense-tracker2-cf7fb.appspot.com",
-  messagingSenderId: "550263037780",
-  appId: "1:550263037780:web:b956ecc51f202e390cf2c1"
+
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
